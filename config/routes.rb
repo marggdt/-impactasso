ba56b238d47fc5bfa20926361165cf97c21abf97
Nona Schneider <nonaschneider99@gmail.com>
2021-03-15 16:36:28 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  ressources :associations do
+    ressources :missions, only: [:new, :create, :delete]
+  end
+
+
+  ressources :users do
+    ressources :favorites, only: :index
+  end
+
+  ressources :missions, only: :show do
+    ressources :favorites, only: :create
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
