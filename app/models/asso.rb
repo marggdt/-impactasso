867868676242b2a397f5f54b2d0939238de7f4d3
margaux gaudriot <margaux.gaudriot@hotmail.fr>
2021-03-17 12:52:56 +0100

    ajout search

--- app/models/asso.rb
+++ app/models/asso.rb
@@ -1,3 +1,8 @@
 class Asso < ApplicationRecord
   has_many :missions
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description, against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
