254c5d49326a2f2590d48af882b6abd3baaf337c
pierre thomas <pierre.thomas@essca.eu>
2021-03-17 15:02:27 +0100

    categories

--- app/controllers/assos_controller.rb
+++ app/controllers/assos_controller.rb
@@ -1,11 +1,24 @@
 class AssosController < ApplicationController
-
   def index
-  	@assos = Asso.all
+   @assos = Asso.all
   end
 
   def show
     @asso = Asso.find(params[:id])
   end
 
+  def new
+    @asso = Asso.new
+  end
+
+  def create
+    @asso = Asso.new(params[:asso])
+    @asso.save
+
+    redirect_to asso_path(@asso)
+  end
+
+  def asso_params
+    params.require(:asso).permit(:name, :description, :address, :zipcode, :city, :longitude, :latitude)
+  end
 end
