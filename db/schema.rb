30675cdbae04018856e67c608a5d0e0e87e4a8b7
pierre thomas <pierre.thomas@essca.eu>
2021-03-18 12:33:13 +0100

    les catégories sont finis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_160059) do
+ActiveRecord::Schema.define(version: 2021_03_18_100240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_03_16_160059) do
     t.string "zipcode"
     t.float "longitude"
     t.float "latitude"
+    t.string "category"
+    t.string "image_url"
   end
 
   create_table "favorites", force: :cascade do |t|
