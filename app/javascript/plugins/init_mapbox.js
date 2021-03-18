16e4990fd4a1a853ad5a25d50794b404c2c2e897
margaux gaudriot <margaux.gaudriot@hotmail.fr>
2021-03-18 15:47:20 +0100

    map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -10,18 +10,19 @@ const fitMapToMarkers = (map, markers) => {
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-
     new mapboxgl.Marker()
-      .setLngLat([marker.lng, marker.lat])
-      .setPopup(popup)
-      .addTo(map);
+    .setLngLat([marker.lng, marker.lat])
+    .setPopup(popup)
+    .addTo(map);
   });
+
 };
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
   if (mapElement) {
+    console.log("in mapox")
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
@@ -29,6 +30,8 @@ const initMapbox = () => {
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
+
+    console.log(markers)
     markers.forEach((marker) => {
       new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
